711763bf30082e4804d262886660255cef2f545b
eddcode123 <edwinmutugi29@gmail.com>
2024-08-08 13:39:58 +0300

    sql script to create a table with specified fields

---
+++ 0x00-MySQL_Advanced/1-country_users.sql
@@ -0,0 +1,8 @@
+-- Check if the table exists and create it if it doesn't with this fileds
+-- id, email, name, country
+CREATE TABLE IF NOT EXISTS users (
+    id int NOT NULL AUTO_INCREMENT PRIMARY KEY,
+    email varchar(255) UNIQUE NOT NULL,
+    name varchar(255)
+    country enum('US', 'CO', 'TN') NOt NULL
+);
