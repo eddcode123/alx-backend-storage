eaab3083539ed315df6e36dc53100d14a7f7714c
eddcode123 <edwinmutugi29@gmail.com>
2024-08-14 20:35:53 +0300

    updating my code

--- 0x02-redis_basic/exercise.py
+++ 0x02-redis_basic/exercise.py
@@ -1,37 +1,107 @@
 #!/usr/bin/env python3
-''' Creating a cache using the Redis module in Python
-'''
+"""
+Redis module
+"""
+import sys
+from functools import wraps
+from typing import Union, Optional, Callable
+from uuid import uuid4
 
 import redis
-import uuid
-from typing import Union
 
 UnionOfTypes = Union[str, bytes, int, float]
 
 
+def count_calls(method: Callable) -> Callable:
+    """
+    a system to count how many
+    times methods of the Cache class are called.
+    :param method:
+    :return:
+    """
+    key = method.__qualname__
+
+    @wraps(method)
+    def wrapper(self, *args, **kwargs):
+        """
+        Wrap
+        :param self:
+        :param args:
+        :param kwargs:
+        :return:
+        """
+        self._redis.incr(key)
+        return method(self, *args, **kwargs)
+
+    return wrapper
+
+
+def call_history(method: Callable) -> Callable:
+    """
+    add its input parameters to one list
+    in redis, and store its output into another list.
+    :param method:
+    :return:
+    """
+    key = method.__qualname__
+    i = "".join([key, ":inputs"])
+    o = "".join([key, ":outputs"])
+
+    @wraps(method)
+    def wrapper(self, *args, **kwargs):
+        """ Wrapp """
+        self._redis.rpush(i, str(args))
+        res = method(self, *args, **kwargs)
+        self._redis.rpush(o, str(res))
+        return res
+
+    return wrapper
+
+
 class Cache:
-    ''' Class to handle caching with Redis.
-    '''
+    """
+    Cache redis class
+    """
 
     def __init__(self):
-        ''' Initializes the Cache instance with a Redis connection
-        and flushes the database.
-        '''
+        """
+        constructor of the redis model
+        """
         self._redis = redis.Redis()
         self._redis.flushdb()
 
+    @count_calls
+    @call_history
     def store(self, data: UnionOfTypes) -> str:
         """
-        Stores a value in the cache with a randomly generated UUID key.
-
-        Args:
-            data (str): The value to be stored in the cache.
-
-        Returns:
-            str: The UUID key used to store the value.
+        generate a random key (e.g. using uuid),
+         store the input data in Redis using the
+          random key and return the key.
+        :param data:
+        :return:
         """
-        # Generate a new UUID key
-        mykey = str(uuid.uuid4())
-        # Store the data in Redis using the generated key
-        self._redis.set(mykey, data)
-        return mykey
+        key = str(uuid4())
+        self._redis.mset({key: data})
+        return key
+
+    def get(self, key: str, fn: Optional[Callable] = None) \
+            -> UnionOfTypes:
+        """
+        convert the data back
+        to the desired format
+        :param key:
+        :param fn:
+        :return:
+        """
+        if fn:
+            return fn(self._redis.get(key))
+        data = self._redis.get(key)
+        return data
+
+    def get_int(self: bytes) -> int:
+        """get a number"""
+        return int.from_bytes(self, sys.byteorder)
+
+    def get_str(self: bytes) -> str:
+        """get a string"""
+        return self.decode("utf-8")
