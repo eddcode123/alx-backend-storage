196170e4d98b94dfabc209f41ba94f3597ac84a1
eddcode123 <edwinmutugi29@gmail.com>
2024-08-14 13:19:05 +0300

    python sctipt that creates a cache and stores a key and value pair and returns the key

---
+++ 0x02-redis_basic/exercise.py
@@ -0,0 +1,34 @@
+#!/usr/bin/env python3
+''' Creating a cache using the Redis module in Python
+'''
+
+import redis
+import uuid
+
+
+class Cache:
+    ''' Class to handle caching with Redis.
+    '''
+
+    def __init__(self):
+        ''' Initializes the Cache instance with a Redis connection
+        and flushes the database.
+        '''
+        self._redis = redis.Redis()
+        self._redis.flushdb()
+
+    def store(self, data):
+        """
+        Stores a value in the cache with a randomly generated UUID key.
+
+        Args:
+            data (str): The value to be stored in the cache.
+
+        Returns:
+            str: The UUID key used to store the value.
+        """
+        # Generate a new UUID key
+        mykey = str(uuid.uuid4())
+        # Store the data in Redis using the generated key
+        self._redis.set(mykey, data)
+        return mykey
