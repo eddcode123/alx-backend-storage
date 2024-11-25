e11c953a2edfedcb97225732ce6f8676de2f0636
eddcode123 <edwinmutugi29@gmail.com>
2024-11-25 15:29:01 +0300

    creating a sql table named 'users' with the following fields(id, email, name, country('us', 'co', 'tn') default of 'us'

--- 0x00-MySQL_Advanced/1-country_users.sql
+++ 0x00-MySQL_Advanced/1-country_users.sql
@@ -4,6 +4,6 @@ CREATE TABLE IF NOT EXISTS users (
     id INT AUTO_INCREMENT NOT NULL,
     email VARCHAR(255) UNIQUE NOT NULL,
     name VARCHAR(255),
-    country ENUM('US', 'CO', 'TN') DEFAULT 'US',
+    country ENUM('US', 'CO', 'TN') DEFAULT 'US' NOT NULL,
     PRIMARY KEY (id)
 );
