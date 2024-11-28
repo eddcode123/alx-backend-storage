48963debacb422239d65e1aec0bcb1ee7abfceff
eddcode123 <edwinmutugi29@gmail.com>
2024-11-28 16:34:09 +0300

    Python function that inserts a new document in a collection based on kwargs

---
+++ 0x01-NoSQL/9-insert_school.py
@@ -0,0 +1,16 @@
+#!/usr/bin/env python3
+"""Insert a document in Python"""
+
+
+def insert_school(mongo_collection, **kwargs):
+    """
+    Inserts a new document in a collection based on kwargs.
+    Args:
+        mongo_collection (MongoCollection): Mongo collection object.
+        kwargs (Key-value pair): Key-value pairs to insert into the document.
+    Returns:
+        id: The ID of the inserted document.
+    """
+    # Insert kwargs in mongo_collection
+    result = mongo_collection.insertMany(kwargs)
+    return result.inserted_ids
