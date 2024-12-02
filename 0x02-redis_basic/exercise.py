cd2f24b930b4b79846efc5ca5f41651584d41597
eddcode123 <edwinmutugi29@gmail.com>
2024-12-02 13:54:06 +0300

    create a cache class that represents a meme cache and stored data using redis

--- 0x02-redis_basic/exercise.py
+++ 0x02-redis_basic/exercise.py
@@ -30,9 +30,9 @@ class Cache:
         key = str(uuid.uuid4())
         self._redis.mset({key: data})
         return key
-    
+
     def get(self, Key: str, fn: Optional[Callable]) \
-        -> unionTypes:
+            -> unionTypes:
         """
         convert the data back
         to the desired format
@@ -40,16 +40,16 @@ class Cache:
             key (str): key to retive value
             fn (Callable): function to convert bytes to string
         Returns:
-            data (unionTypes): data associated to the key 
+            data (unionTypes): data associated to the key
         """
         if fn:
             return fn(self._redis.get(Key))
         return self._redis.get(Key)
-    
+
     def get_str(self: bytes) -> str:
         """Get string"""
         return self.decode('utf-8')
-    
+
     def get_int(self: bytes) -> int:
         """Get integer"""
         return int(self)
