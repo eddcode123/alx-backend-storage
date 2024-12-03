ac4d3c776d68fb206ba04d58b8644683d6ee61b9
eddcode123 <edwinmutugi29@gmail.com>
2024-12-03 06:12:39 +0300

    create a cache class that represents a meme cache and stored data using redis

--- 0x02-redis_basic/exercise.py
+++ 0x02-redis_basic/exercise.py
@@ -2,11 +2,40 @@
 """Writing strings to Redis"""
 
 import redis
+from functools import wraps
 import uuid
 from typing import Union, Callable, Optional
 import sys
 
 unionTypes = Union[str, bytes, int, float]
+
+
+def count_calls(method: callable) -> callable:
+    """
+    Decorator to count the number of times a method is called.
+    Stores the count in a Redis database using the method's qualified name.
+    Args:
+        method (callable): The method to be wrapped.
+    Returns:
+        callable: The wrapped method with call counting.
+    """
+    key = method.__qualname__
+
+    @wraps(method)
+    def wrapper(self, *args, **kwargs):
+        """
+        Wrapper function to increment the call count in Redis
+        before executing the original method.
+        Args:
+            self: The instance of the class containing the method.
+            *args: Positional arguments for the method.
+            **kwargs: Keyword arguments for the method.
+        Returns:
+            The return value of the original method.
+        """
+        self._redis.incr(key)
+        return method(self, *args, *kwargs)
+    return wrapper
 
 
 class Cache:
@@ -20,6 +49,7 @@ class Cache:
         # flush the instance
         self._redis.flushdb()
 
+    @count_calls
     def store(self, data: unionTypes) -> str:
         """Method that stores the input data in Redis
         using the random key and return the key
@@ -38,15 +68,15 @@ class Cache:
         convert the data back
         to the desired format
         Args:
-            key (str): key to retive value
+            key (str): key to retrive value
             fn (Callable): function to convert bytes to string
         Returns:
-            data (unionTypes): data associated to the key
+            data (unionTypes): Value associated to the key
         """
         if fn:
             return fn(self._redis.get(Key))
         return self._redis.get(Key)
-    
+
     def get_int(self: bytes) -> int:
         """get a number"""
         return int.from_bytes(self, sys.byteorder)
